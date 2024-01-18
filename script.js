ed88520e110b2c864698af33bcbc2274872aec5f
KingShakeJS <kingshakejs@yandex.ru>
2024-01-18 13:47:31 +0300

    shag-35

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let xp = 0;
 let health = 100;
 let gold = 50;
-let currentWeapon=0;
+let currentWeapon = 0;
 let fighting;
 let monsterHealth;
-let inventory=["stick"];
+let inventory = ["stick"];
 
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
@@ -15,3 +15,4 @@ const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth ");
